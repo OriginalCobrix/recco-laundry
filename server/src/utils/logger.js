25ae9605625fbdadfb158ba44146c937fb2e4a38
Subhan9990 <noshabanaz12@gmail.com>
2026-07-09 11:15:34 +0500

    Fix logger for railway console output

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -7,15 +7,16 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
+    // Console transport hamesha on rakho taaki Railway logs dikhein
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' })
   ]
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
 
 module.exports = logger;
